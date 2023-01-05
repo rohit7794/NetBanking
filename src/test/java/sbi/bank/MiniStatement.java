30d4eb495d9e1c779297a3a5de11d309bdf6a9a1
rohit7794 <patilrohit7794@gmail.com>
2023-01-05 16:41:11 +0530

    third commit

--- src/test/java/sbi/bank/MiniStatement.java
+++ src/test/java/sbi/bank/MiniStatement.java
@@ -3,27 +3,23 @@ package sbi.bank;
 public class MiniStatement {
 
 	public static void main(String[] args) {
-	
 
 	}
+
 	public static void miniStatement() {
-		
+
 		System.out.println("first 5 transcation by rohit");
-<<<<<<< HEAD
-		System.out.println("this line written by rohit");
-		
-=======
-		System.out.println("2nd line by triveni");
->>>>>>> be099d665485485ae36d3ea25126fd56642e5f37
-		
+
 	}
+
 	public static void currentAccount() {
-		
+
 		System.out.println("this account by rohit");
-		if(true) {
-			
+		System.out.println("this line written by rohit1");
+		if (true) {
+
 			System.out.println("if else statement");
-			System.out.println("else statement by triveni");
+			System.out.println("this line written by rohit2");
 		}
 	}
 
