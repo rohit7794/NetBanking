398577865ce6ddf8f87994cb82189fb91d6963dc
rohit7794 <patilrohit7794@gmail.com>
2023-01-05 15:02:14 +0530

    mini statement test

---
+++ src/test/java/sbi/bank/MiniStatement.java
@@ -0,0 +1,16 @@
+package sbi.bank;
+
+public class MiniStatement {
+
+	public static void main(String[] args) {
+	
+
+	}
+	public static void miniStatement() {
+		
+		System.out.println("first 5 transcation by rohit");
+		
+		
+	}
+
+}
